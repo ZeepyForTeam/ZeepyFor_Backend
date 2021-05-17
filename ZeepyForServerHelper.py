bd69728c7d09d92c212cdb0c755bfe3c53179b6c
koreaaerospaceU <zx811@naver.com>
2021-05-18 01:27:57 +0900

    feat : Zeepy 서버 API 호출 함수 추가

--- ZeepyForServerHelper.py
+++ ZeepyForServerHelper.py
@@ -7,12 +7,26 @@ class zeepy_for_server_helper:
     def __init__(self):
         self.zeepy_for_server_url = "http://localhost:8080"
 
-    def update_building(self, data):
+    def upload_building(self, data):
         url = self.zeepy_for_server_url + "/api/building"
         headers = {'Content-Type': 'application/json; charset=utf-8'}
-        requests.post(url, data=json.dumps(data), headers=headers)
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        return response
 
-    def update_building_deal(self, data):
+    def upload_building_deal(self, data):
         url = self.zeepy_for_server_url + "/api/deal"
         headers = {'Content-Type': 'application/json; charset=utf-8'}
-        requests.post(url, data=json.dumps(data), headers=headers)
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        return response
+    
+    def update_building_deal(self, data, _id):
+        url = self.zeepy_for_server_url + "/api/deal/" + str(_id)
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        response = requests.put(url, data=json.dumps(data), headers=headers)
+        return response
+
+    def get_building_deal(self, _id, floor):
+        url = self.zeepy_for_server_url + "/api/deal/floor/" + str(floor) + "/building/" + str(_id) 
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        response = requests.get(url, headers=headers)
+        return response
